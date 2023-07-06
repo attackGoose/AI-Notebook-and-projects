12bb76495574e9fd4b41225f3dc09e5f11cfd132
attackGoose <henrycao0308@gmail.com>
2023-07-06 18:16:39 -0400

    added more tags cus y not

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import tensorflow
 
 #using this series to learn: https://www.youtube.com/watch?v=wypVcNIH6D4&list=PLzMcBGfZo4-ndH9FoC4YWHGXG5RZekt-Q&index=1&t=0s
 #note, you have to learn the intent of a message, hence "intents" is meant to give repsonses to what vibe the program believes the conversation is giving off, refer to tutorial
+
+
