12b76a1fd1ca37b893f864773c459fb446833e0e
attackGoose <henrycao0308@gmail.com>
2023-07-06 17:40:02 -0400

    added a chatlog and updated intents.json

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import tensorflow
 
+#note, you have to learn the intent of a message, hence "intents" is meant to give repsonses to what vibe the program believes the conversation is giving off, refer to tutorial
