f3da0326c3f090f4913645ac4cc1db328b40a6e4
attackGoose <henrycao0308@gmail.com>
2023-07-09 19:43:47 -0400

    still following tutorials, nothing should work yet added some stuff as well

--- pytorch/Learning_pytorch.py
+++ pytorch/Learning_pytorch.py
@@ -1,10 +1,32 @@
 #this file is for learning pytorch, and choosing if i like tensorflow or pytorch more
+#https://pytorch.org/tutorials/
 #https://youtu.be/V_xro1bcAuA
 
 
 import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(torch.__version__)
+
+### creating a tensor
+scalar = torch.Tensor(7) #has no dimensions, one number
+
+print(scalar.shape) #returns the number we gave it
+
+vector = torch.Tensor([7, 7]) #has 1 dim since its a 1 dim array
+print(vector.shape)
+
+MAXTRIX = torch.Tensor([[7, 8],
+                        [9, 10]])
+
+TENSOR = torch.Tensor([[[1,2,3],
+                        [3,6,9],
+                        [2,4,5]]])
+print(TENSOR.shape)
 
 
+### random tensors: (creates more accurate training data)
 
-
-#i'm not going to use this file until after i finish, prob as like a add on
+rand_tensor = torch.rand(3, 4) #creates a tensor of size 3, 4 (matrix) 
--- tensorflow/testing stuff/Testing_text_generation.py
+++ tensorflow/testing stuff/Testing_text_generation.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Activation
+from keras.optimizers import RMSprop
 
 import numpy as np
 import os
@@ -9,16 +13,28 @@ path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googl
 text = open(path_to_file, "rb").read().decode(encoding="utf-8")
 
 vocab = sorted(set(text))
-print(f"{len(vocab)} unique characters")
 
 #print(f"Length of test: {len(text)} characters")
 
 #changing the letters to a numerical representation before testing:
-example_texts = ['abcdefg', 'xyz']
+sample_characters = ['abcdefghijklmnopqrstuvw', 'xyz']
 
-chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
-print(chars)
+#creates tokens from the list of letters
+chars = tf.strings.unicode_split(sample_characters, input_encoding='UTF-8')
 
 ids_from_chars = tf.keras.layers.StringLookup(
     vocabulary=list(vocab), mask_token=None)
 
+chars = ids_from_chars(chars)
+
+
+chars_from_ids = tf.keras.layers.StringLookup(
+    vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+
+tf.strings.reduce_join(chars, axis=-1).numpy()
+
+
+def text_from_ids(ids):
+  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
