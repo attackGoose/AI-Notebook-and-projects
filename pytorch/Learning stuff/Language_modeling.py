513cc66c93359892244442e65b4a8fe390e1d964
attackGoose <henrycao0308@gmail.com>
2023-07-10 16:36:22 -0400

    super minor update, updated notes

---
+++ pytorch/Learning stuff/Language_modeling.py
@@ -0,0 +1,3 @@
+#https://www.youtube.com/watch?v=kCc8FmEb1nY&t=4s
+
+#using a transformer to build a LLM (large langauge model)
--- tensorflow/testing stuff/Testing_text_generation.py
+++ tensorflow/testing stuff/Testing_text_generation.py
@@ -25,11 +25,14 @@ chars = tf.strings.unicode_split(sample_characters, input_encoding='UTF-8')
 ids_from_chars = tf.keras.layers.StringLookup(
     vocabulary=list(vocab), mask_token=None)
 
-chars = ids_from_chars(chars)
+ids = ids_from_chars(chars)
 
 
 chars_from_ids = tf.keras.layers.StringLookup(
     vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+
+chars = chars_from_ids(ids)
+
 
 tf.strings.reduce_join(chars, axis=-1).numpy()
 
@@ -38,3 +41,48 @@ def text_from_ids(ids):
   return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
 
 all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
+
+seq_length = 100
+
+sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
+
+for seq in sequences.take(5):
+  print(text_from_ids(seq).numpy())
+
+
+def split_input_target(sequence):
+  input_text = sequence[:-1]
+  target_text = sequence[1]
+  return input_text, target_text
+
+split_input_target(list("tensorflow"))
+
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in dataset.take(1):
+    print("Input :", text_from_ids(input_example).numpy())
+    print("Target:", text_from_ids(target_example).numpy())
+
+
+
+#creating training batches:
+
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = (
+    dataset
+    .shuffle(BUFFER_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True)
+    .prefetch(tf.data.experimental.AUTOTUNE))
+
+print(dataset)
