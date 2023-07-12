c872f07f4dfa6ea11d6592d3ad412321c05d3514
attackGoose <henrycao0308@gmail.com>
2023-07-12 18:35:13 -0400

    saving progress

--- pytorch/Learning stuff/Learning_pytorch_workflow.py
+++ pytorch/Learning stuff/Learning_pytorch_workflow.py
@@ -7,13 +7,13 @@ from torch import nn #nn contains all of pytorch's building blocks for neuro net
 
 import matplotlib.pyplot as plt
 
+"""
+preparing and loading data (data can be almost anything in machine learning, like images, csv, videos, audio, text, or even dna)
 
-## preparing and loading data (data can be almost anything in machine learning, like images, csv, videos, audio, text, or even dna)
-
-# machine learning is a game of 2 major parts: (that can be further subdivided into many other parts)
-# 1. get data into a numerical representation (tensors)
-# 2. build a model to learn patterns in that numerical representation
-
+machine learning is a game of 2 major parts: (that can be further subdivided into many other parts)
+1. get data into a numerical representation (tensors)
+2. build a model to learn patterns in that numerical representation
+"""
 # making data using a linear regression formula:
 
 #creating known parameters:
@@ -24,6 +24,90 @@ bias = 0.3
 start = 0
 end = 1
 step = 0.02
-X = torch.arange(start, end, step).unsqueeze(dim=1) #x is usually used as a tensor 
-y = weight * X + bias
-print(X)
+X = torch.arange(start, end, step).unsqueeze(dim=1) #x is usually used as a tensor, and we need the extra dimension for something later
+y = weight * X + bias #the machine won't know this and will have to figure this out for itself, the y variable is the target
+
+print(X[:10], y[:10], len(X), len(y))
+
+
+## spliting data into training and test sets (one of the most important concepts in machine learning in general)
+
+"""
+visualizing the three datasets by comparing it to a school course:
+
+training set: you can compare this to the course materials at a university that you would learn throughout the year, the model too would learn patterns from here
+validation set: you can compare this to a practice exam, which would tune the model patterns/adjust the model's patterns (not always needed)
+Test set: you can compare this to a final exam: which would see if the model is ready to be implimented/tests the model's performance on data it hasn't seen before
+
+Goal: generalization (the ability for a machine learning model to perform well on data it hasn't seen before)
+
+amount of data used for training set: ~60-80% (always needed)
+amount of data used for validation set: 10-20% (not always needed)
+amount of data used for test set: 10-20% (always needed)
+"""
+
+#create a train/test split/set (set and split mean the same thing in this case)
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split] #gets all the data that's previous to that index
+X_test, y_test = X[train_split:], y[train_split:] #gets all the data that is past that index
+
+print(len(X_train), len(y_train), len(X_test), len(y_test)) #prints the amount of training features, training lables, testing features, testing lables
+
+#NOTE: you can also use the sklearn/scikit module to split the training data in a more random way
+
+
+## building a function to visualize the data
+
+def plot_prediction(train_data = X_train, 
+                    train_lables = y_train, 
+                    test_data = X_test, 
+                    test_lables = y_test, 
+                    predictions = None):
+    """
+    Plots training data, test data, and compares predictions
+    """
+    plt.figure(figsize=(10, 7))
+
+    #plot training data in blue
+    plt.scatter(train_data, train_lables, c="blue", s=4, label="Training Data")
+    
+    #plot testing data in green
+    plt.scatter(test_data, test_lables, c="green", s=4, label="Testing Data")
+
+    if predictions != None:
+        #plot the predictions if they exist
+        plt.scatter(test_data, predictions, c="red", s=4, label="Predictions")
+
+    plt.legend(prop={"size": 14})
+
+    plt.show()
+
+plot_prediction()
+
+#building a model:
+
+class LinearRegressionModel(nn.Module): # <- almost everything in pytorch inherits from nn, for more info: https://pytorch.org/docs/stable/generated/torch.nn.Module.html
+    def __init__(self):
+        super().__init__() #start with random parameters, then update them to fit the training data, by running it through the formula it'll adjust the data to fit the linear regression formula
+        self.weight = nn.Parameter(torch.randn(1,
+                                              requires_grad=True, #gradient descent = true
+                                              dtype=torch.float))
+        self.bias = nn.Parameter(torch.randn(1,
+                                             requires_grad=True,
+                                             dtype=torch.float))
+        
+    # Forward method to define the computation in a model:
+    def forward(self, x: torch.Tensor) -> torch.Tensor: #x is the input data (of torch.Tensor datatype), and this function is going to return a tensor datatype 
+        return self.weight * x + self.bias #this is the linear regression formula
+
+"""
+what the model does:
+ - Starts with random values (weights and biases)
+ - looks at training data and adjusts the random values to better represent/get closer to the ideal values (weight and bias values of our original formula)
+
+How does it do it:
+ 1. Gradient Descent
+ 2. Back Propagation 
+"""
+
+#Current time stamp: 5:07:31
