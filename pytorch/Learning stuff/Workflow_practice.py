858dbd76b3c760726257df4b52d3908c35a27452
attackGoose <henrycao0308@gmail.com>
2023-07-16 19:40:31 -0400

    attempted to resolve a bug, got stuck on another bug

--- pytorch/Learning stuff/Workflow_practice.py
+++ pytorch/Learning stuff/Workflow_practice.py
@@ -17,27 +17,72 @@ step = 0.02
 X = torch.arange(start, end, step)
 y = weight * X + bias
 
-train_split = int(0.8*len(X))
+train_split = int(0.8 * len(X))
 X_train, y_train = X[:train_split], y[:train_split]
 X_test, y_test = X[train_split:], y[train_split:]
 
 ##Building the model:
-
 class LinearRegressionModel(nn.Module):
-    def __init__(self) -> None:
-        super().__init__() #this time, we're not initializing the parameters outselves, this time we're initializing the layer using the Linear to make things easier for outselves
+    def __init__(self):
+        super().__init__()
+        #this time, we're not initializing the parameters outselves, this time we're initializing the layer using the Linear to make things easier for outselves
         #this layer is also called the linear transform, probing layer, fully connected layer, dense layer, or etc
         #usually these layers will be created using precreated layers in pytorch rather than creating the layers yourself using nn.Parameter()
         self.linear_layer = nn.Linear(in_features=1,
-                                      out_features=1,)
-        
-
+                                      out_features=1)
+    
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.linear_layer
-
-MANUAL_SEED = torch.manual_seed(42)
+        return self.linear_layer(x)
 
 model = LinearRegressionModel()
 
 print(model, list(model.state_dict()))
+
+#setting the data and model to use the target device:
+model.to(device=device)
+
+print(f"x_train size: {X_train.shape} | x_test size: {X_test.shape} | y_train size: {y_train.shape} | y_test size: {y_test.shape}")
+
+##Training:
+"""
+we need a loss function
+optimizer
+training loop
+testing loop
+"""
+
+loss_func = nn.L1Loss()
+
+optimizer = torch.optim.SGD(params=model.parameters(),
+                            lr=0.05)
+
+epoches = 200
+
+#training loop:
+
+for epoch in range(epoches):
+    model.train()
+
+    y_prediction = model(X_train)
+
+    #creates a loss value based on difference in predicted value and the expected value
+    loss = loss_func(y_prediction, y_train)
+
+    #reset gradients
+    optimizer.zero_grad()
+
+    #back prop
+    loss.backward()
+
+
+    optimizer.step()
+
+    model.eval()
+    with torch.inference_mode():
+        test_pred = model(X_test)
+
+        test_loss = loss_func(test_pred, y_test)
+
+    if epoch % 10 == 0:
+        print(f"epoch = {epoch} | loss = {loss} | test loss = {test_loss}")
 
