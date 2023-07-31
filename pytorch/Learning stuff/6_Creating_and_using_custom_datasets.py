58bc981e129379305846e5258e0f25c38cff5378
attackGoose <henrycao0308@gmail.com>
2023-07-31 13:39:47 -0400

    made and loaded a custom dataset to use in pytorch

--- pytorch/Learning stuff/6_Creating_and_using_custom_datasets.py
+++ pytorch/Learning stuff/6_Creating_and_using_custom_datasets.py
@@ -11,12 +11,13 @@
 import torch
 from torch import nn
 
-import torchvision
-
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
+import matplotlib.pyplot as plt
+import numpy as np
 #getting the custom dataset (starting on a small scale then increasing the scale when necessary (speeding up experiments))
 # (subset of the food 101 dataset that only has 3 categories of food and 10 percent of the images, so around 75 training images per class and 25 testing images per class):
 
@@ -46,3 +47,162 @@ else:
         print("unzipping image data")
         #extracts data from the zipfile into the other file
         zip_ref.extractall(image_path)
+
+# data preparation and exploration
+
+import os
+
+def walk_through_dir(dir_path):
+    """walks through dir_path, returning its contents."""
+
+    for dirpath, dirnames, filenames in os.walk(dir_path): #hover over the .walk() function to see what it does incase I forget
+        print(f"There are {len(dirnames)} directories and {len(filenames)} imgaes in {dirpath}") #this shows how many test images and train images are in its corresponding path
+
+# Setup train and testing paths
+train_dir = image_path / "train"
+test_dir = image_path / "test"
+
+
+#standard image classification data format:  (found on torchvision documentation)
+"""
+pizza_sushi_steak/ <- overall dataset folder
+    train/ <- train dataset folder
+        pizza/ <- class of the image
+            image1.png
+            image2.png
+            ...
+        sushi/ <- class of the image
+            image1.png
+            image2.png
+            ...
+        steak/ <- class of the image
+            image1.png
+            image2.png
+            ...
+        ...
+    test/ <- test dataset folder
+        pizza/  <- class of the image
+            image1.png
+            image2.png
+            ...
+        sushi/ <- class of the image
+            image1.png
+            image2.png
+            ...
+        steak/ <- class of the image
+            image1.png
+            image2.png
+            ...
+        ...
+"""
+
+"""
+steps for what to do next:
+1. get all image paths
+2. pick a random image path using random.choice()
+3. get image's class name using pathlib.Path.parent.stem
+4. open the image with "pillow" for image processing/manupulation
+5. show image and print metadata
+"""
+
+import random
+from PIL import Image
+
+#setting random seed:
+random.seed(42)
+
+#1. getting all the imgae paths:
+image_path_list = list(image_path.glob('*/*/*.jpg')) #glob is basically globbing together all of the images that are in a certain path and fit the criteria
+                                                    #the * means it could be anything
+
+random_image_path = random.choice(image_path_list)
+
+image_class = random_image_path.parent.stem
+
+#opening the imgae)
+img = Image.open(random_image_path)
+
+# printing image's metadata
+print(f"Random image path: {random_image_path}\nImage class: {image_class}\nImage height: {img.height}\nImage width: {img.width}\nImage: {img}")
+
+#visualizing the image data
+
+img_as_array = np.asarray(img) #turning image into array
+
+#showing the image using matplotlib
+plt.figure(figsize=(10, 7))
+plt.imshow(img_as_array)
+plt.title(f"Image class: {image_class} | Image shape: {img_as_array.shape} -> [height, width, color channels]") #this is the order of the image properties for matplotlib (diff for pytorch)
+plt.axis(False)
+plt.show()
+
+print(img_as_array)
+
+"""
+steps to use the image with pytorch:
+1. turn the target data into tensors (in this case its the numerical representation of images)
+2. then turn it into a torch.utils.data.Dataset (like in the previous unit) then subsequently into a torch.utils.data.DataLoader to use (respectively called Dataset and DataLoader)
+"""
+
+#transforming data using torchvision.transforms
+data_transform = transforms.Compose([
+    #resizing the imgae
+    transforms.Resize(size=(64, 64)),
+    #flip the imgaes randomly on the horizontal 
+    transforms.RandomHorizontalFlip(p=0.5),
+    # turn the igae into a torch tensor
+    transforms.ToTensor()
+])
+
+data_transform(img) #passing in a PIL image
+print(data_transform(img).shape)
+
+def plot_transformed_images(image_paths:list, transform, n=3, seed=None):
+    """
+    selects random images from a path of images and loads/transforms them then plots the original v transformed version
+    """
+    if seed:
+        random.seed(seed)
+    random_image_path = random.sample(image_paths, k=n)
+    for image_path in random_image_path:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(nrows=1, ncols=2)
+            ax[0].imshow(f)
+            ax[0].set_title(f"Original\nSize: {f.size}")
+            ax[0].axis(False)
+
+            #transform and plot the target image
+            transformed_image = transform(f).permute(1, 2, 0) # note that we will need to change the shape for matplotlib
+            ax[1].imshow(transformed_image)
+            ax[1].set_title(f"Transformed\nSize: {transformed_image.shape}")
+            ax[1].axis(False)
+
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+
+plot_transformed_images(image_paths=image_path_list,
+                        transform=data_transform,
+                        n=3, #number of imgaes to do this on
+                        )
+
+plt.show()
+
+
+"""
+options for loading data:
+
+loading images using "ImageFolder" (torchvision.datasets.ImageFolder)
+"""
+
+train_data = datasets.ImageFolder(root=train_dir, #it will look at this for its label/target
+                                  transform=data_transform, #a transform for the data
+                                  target_transform=None) #a transform for the label/target
+
+test_data = datasets.ImageFolder(root=test_dir,
+                                 transform=data_transform,
+                                 target_transform=None)
+
+print(train_data, test_data)
+
+#the above method will produce a torch dataset
+
+#timestamp: 21:05:40
